28a58a4c9610d30e4273a80f458201be7e823b8b
mrpatromax <mrpatromax99@gmail.com>
2023-04-03 19:12:49 +0530

    some js stuff

---
+++ first_project.js
@@ -0,0 +1,24 @@
+console.log("hello form js ");
+var name="vansh";
+var age=18;
+console.log(name,age);
+var okay=true;
+console.log(okay)
+var array=[1,2,3,4,5,6,7,8,9,0]
+console.log(array)
+console.log(array[2])
+var obj = {
+    name: "vansh",
+    age: 18,
+    test: ["pass","fail","pass"],
+    exam : {
+        mid: "pass",
+        end: "pass"
+    },
+    "full name": "vansh kumar"
+}
+console.log(obj);
+console.log(obj.name);
+console.log(obj.exam);
+console.log(obj.exam.mid)
+console.log(obj["full name"])
